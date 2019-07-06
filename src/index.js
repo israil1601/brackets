5e34460646480d2c1d8e8cee484db7b73d475900
israil1601 <israil.salikhodjaev@gmail.com>
2019-07-06 15:01:45 +0500

    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	var changed = false
+	for (var i = 0; i < bracketsConfig.length; i++) {
+		if (~str.indexOf(bracketsConfig[i].join(''))) {
+			str = str.replace(bracketsConfig[i].join(''), '')
+			changed = true
+		}
+	}
+	if (str && changed) {
+		return check(str, bracketsConfig)
+	}
+	else { return str ? false : true }
 }
