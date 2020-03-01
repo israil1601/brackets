e01227a706218f663eba5e433434c31d10edeca5
israil1601 <israil.salikhodjaev@gmail.com>
2020-03-01 14:28:55 +0000

    test2

--- example.js
+++ example.js
@@ -1178,6 +1178,7 @@ exports.handler = (event, context, callback) => {
         {'source': '^/Hello from SQS!'},
         {'source': '^/Hello from SQS!'},
         {'source': '^/Hello from SQS!'},
+        {'source': '^/Hello from SQS!'},
 
 	];
 	for (let i = 0; i < oldSiteUris.length; i++) {
